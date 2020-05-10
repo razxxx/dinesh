93e6e048e240c5be92707e0da063f0db36a2710e
DineshkumarT <DineshkumarT@IN-O2-LPT035.thedigitalgroup.com>
2020-05-10 15:20:30 +0530

    this is my first commit

---
+++ src/main/java/din/com/TestSimple.java
@@ -0,0 +1,18 @@
+package din.com;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestSimple {
+
+	@Test
+	public void simpleTestPass() {
+		int a = 1;
+		int b = 3;
+		
+		assertTrue(a + b== 4);
+		
+	}
+	
+}
